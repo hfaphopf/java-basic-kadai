8a5743aeeb2ef4b66299311a5971308a42ceb892
hfaphopf <kuma.shou0925@gmail.com>
2023-11-08 16:37:31 +0900

    Price_Chapter08.javaを編集

--- kadai_008/Price_Chapter08.java
+++ kadai_008/Price_Chapter08.java
@@ -1,1 +1,47 @@
+package Price_Chapter08;
+
+public class Price_Chapter08 {
+
+	public static void main(String[] args) {
+		
+		// 変数を宣言
+		int userAge = 30;
+		int serviceCost =3000;
+
+		// 年代に応じた使用料金を取得
+				switch(userAge) {
+				case 10:
+					System.out.println("1000");
+					break;
+				case 20:
+					System.out.println("2000");
+					break;
+				case 30:
+					System.out.println("3000");
+					break;
+				case 40:
+					System.out.println("3000");
+					break;
+				case 50:
+					System.out.println("4000");
+					break;
+				case 60:
+					System.out.println("4000");
+					break;
+				case 70:
+					System.out.println("4000");
+					break;
+				case 80:
+					System.out.println("5000");
+					break;
+				default:
+					System.out.println("500");
+					break;
+				
+				}
+				
+				System.out.println(userAge + "代の料金は" + serviceCost + "円");
+			}
+
+		}
 
