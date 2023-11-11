b2ff40054bc3f20ef6131eb2ab052a9f6d8bc9c1
hfaphopf <kuma.shou0925@gmail.com>
2023-11-11 10:15:55 +0900

    CarExec_Chapter14.javaを編集

--- kadai_014/CarExec_Chapter14.java
+++ kadai_014/CarExec_Chapter14.java
@@ -1,1 +1,16 @@
+package kadai_014;
+
+public class CarExec_Chapter14 {
+
+	public static void main(String[] args) {
+		// Car_Chapter14をインポート
+		Car_Chapter14 car = new Car_Chapter14();
+
+		// メソッドを実行
+		car.gearChange(3);
+		car.run();
+		
+	}
+
+}
 
